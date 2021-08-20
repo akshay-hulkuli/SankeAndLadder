8230a753a19dc185cb370fd141dba389cf0daceb
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-20 18:39:19 +0530

    Initial Upload

---
+++ src/com/bridgelabz/slgame/SLgame.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.slgame;
+
+public class SLgame {
+	public static void main(String[] args) {
+		System.out.println("Snake and Ladder Game played with single player started at position 0");
+	}
+
+}
