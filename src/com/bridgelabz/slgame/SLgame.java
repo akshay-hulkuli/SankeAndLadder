8de5e50a0dae0e8cee12c275edf98996cfea6527
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-20 18:57:38 +0530

    MODIFIED based on UC2

--- src/com/bridgelabz/slgame/SLgame.java
+++ src/com/bridgelabz/slgame/SLgame.java
@@ -2,7 +2,12 @@ package com.bridgelabz.slgame;
 
 public class SLgame {
 	public static void main(String[] args) {
+		
 		System.out.println("Snake and Ladder Game played with single player started at position 0");
+		
+		int dValue = ((int)Math.floor(Math.random()*100) % 6)+1;
+		System.out.println("the value of rolled die is: "+dValue);
+		
 	}
 
 }
